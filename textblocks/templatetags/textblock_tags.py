292ab87456333b198d86b493aa29596fe5b1fe2d
Nico Bernold <nb@feinheit.ch>
2017-12-14 10:19:45 +0100

    fix parentheses bug which prevents display of keys through setting

--- textblocks/templatetags/textblock_tags.py
+++ textblocks/templatetags/textblock_tags.py
@@ -39,7 +39,7 @@ def textblock(key, type='text/plain', show_key='not_set'):
 
     text = textblock.content
     if not text:
-        if show_key != 'not_set' and (show_key or conf.SHOWKEY):
+        if (show_key != 'not_set' and show_key) or conf.SHOWKEY:
             text = textblock.key
 
     # render
