52ca72a716047a2d94d5d781552067b914ad166f
benzkji <bnzk@bnzk.ch>
2018-02-05 11:20:03 +0100

    state filter first version

--- textblocks/admin.py
+++ textblocks/admin.py
@@ -1,7 +1,10 @@
 from __future__ import unicode_literals
 
 from django.apps import apps
+from django.conf import settings
+from django.db.models import Q
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
 from .forms import TextBlockAdminForm
 from .models import TextBlock
@@ -13,10 +16,56 @@ else:
     BaseAdmin = admin.ModelAdmin
 
 
+class TranslationStateFilter(admin.SimpleListFilter):
+    title = _('Translation State')
+    parameter_name = 'translation_state'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('untranslated', _('Untranslated')),
+            ('partially', _('Partiallly')),
+            ('translated', _('Translated')),
+        )
+
+    def queryset(self, request, queryset):
+        if apps.is_installed('modeltranslation'):
+            langs = getattr(settings, 'MODELTRANSLATION_LANGUAGES', settings.LANGUAGES)
+            lang_keys = [lang[0] for lang in langs]
+            q_objects = Q()
+            if self.value() == 'untranslated':
+                for lang_key in lang_keys:
+                    kwargs = {'content_{}'.format(lang_key): ''}
+                    q_objects &= Q(**kwargs)
+                return queryset.filter(q_objects)
+            if self.value() == 'partially':
+                # exclude translated
+                for lang_key in lang_keys:
+                    kwargs = {'content_{}'.format(lang_key): ''}
+                    q_objects |= Q(**kwargs)
+                queryset = queryset.filter(q_objects)
+                # AND exclude untranslated
+                q_objects = Q()
+                for lang_key in lang_keys:
+                    kwargs = {'content_{}'.format(lang_key): ''}
+                    q_objects &= Q(**kwargs)
+                return queryset.exclude(q_objects)
+            if self.value() == 'translated':
+                for lang_key in lang_keys:
+                    kwargs = {'content_{}'.format(lang_key): ''}
+                    q_objects |= Q(**kwargs)
+                return queryset.exclude(q_objects)
+        else:
+            if self.value() == 'partially' or self.value() == 'translated':
+                return queryset.exclude(content='')
+            if self.value() == 'untranslated':
+                return queryset.filter(content='')
+        return queryset
+
+
 class TextBlockAdmin(BaseAdmin):
     list_display = [
         'key', 'type', 'shortened_content', 'accessed_at', 'created_at']
-    list_filter = ['type']
+    list_filter = ['type', TranslationStateFilter]
     fields = ['key', 'content', 'type', 'created_at', 'accessed_at']
     search_fields = ['key', 'content']
     readonly_fields = ['key', 'type', 'created_at', 'accessed_at']
