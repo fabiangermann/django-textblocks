fcfb1e3d11e846ec336bec94716d6d2d2543e169
benzkji <bnzk@bnzk.ch>
2018-05-25 14:03:17 +0200

    adding working admin tests

--- tests/testapp/test_admin.py
+++ tests/testapp/test_admin.py
@@ -1,15 +1,17 @@
+# encoding: utf-8
 from __future__ import absolute_import, unicode_literals
+
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 try:
     reload
 except NameError:
     from importlib import reload
 
-from django.test import TestCase
+from django.test import TestCase, Client
 
-from textblocks import conf
 from textblocks.models import TextBlock
-from textblocks.templatetags.textblock_tags import textblock
 
 
 class TextblocksTest(TestCase):
@@ -31,8 +33,10 @@ class TextblocksTest(TestCase):
     def tearDown(self):
         pass
 
-    def test_has_css(self):
+    def test_renders_non_ascii_into_ckeditor(self):
         self.client.login(username='fred', password='test')
-        url = reverse('admin:textblocks_textblock_changelist')
+        obj = TextBlock(key='test', content='ö$ä-what', type='text/html', )
+        obj.save()
+        url = reverse('admin:textblocks_textblock_change', args=(obj.id, ))
         response = self.client.get(url, follow=True)
-        self.assertContains(response, 'admin/aasdvadsvasdyle.css')
+        self.assertContains(response, 'ö$ä')
