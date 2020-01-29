6ea15be201c00d831cf58fb51d4b8a2ee1248d37
Matthias Kestenholz <mk@feinheit.ch>
2020-01-29 09:27:12 +0100

    Replace ugettext* with gettext*, add a six dependency

--- textblocks/models.py
+++ textblocks/models.py
@@ -1,13 +1,14 @@
 from __future__ import unicode_literals
 
 import hashlib
-import six
 
 from django.conf import settings
 from django.core.cache import cache
 from django.db import models
 from django.utils import timezone
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
+
+import six
 
 from . import conf
 
